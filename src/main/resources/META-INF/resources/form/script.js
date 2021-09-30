53fa574213fa5e8c6ab6497da169c9d74f27eb0d
Lorena Tassi <loritassi@gmail.com>
2021-09-30 09:43:44 +0200

    Login finished, started register

--- src/main/resources/META-INF/resources/form/script.js
+++ src/main/resources/META-INF/resources/form/script.js
@@ -1,5 +1,8 @@
 const URL = 'http://localhost:8080';
 let entries = [];
+let jiras = [];
+let breaks = [];
+let categories = [];
 
 const dateAndTimeToDate = (dateString, timeString) => {
     return new Date(`${dateString}T${timeString}`).toISOString();
@@ -11,6 +14,15 @@ const createEntry = (e) => {
     const entry = {};
     entry['checkIn'] = dateAndTimeToDate(formData.get('checkInDate'), formData.get('checkInTime'));
     entry['checkOut'] = dateAndTimeToDate(formData.get('checkOutDate'), formData.get('checkOutTime'));
+
+    const jiraStat = {};
+    jiraStat['jiraNumber'] = formData.get('jnumber');
+    jiraStat['achievement'] = formData.get('achievement')
+
+    const category = {};
+    category['text'] = formData.get('admin');
+    category['text'] = formData.get('itsupport');
+    category['text'] = formData.get('projekt');
 
     fetch(`${URL}/entries`, {
         method: 'POST',
@@ -57,7 +69,55 @@ const renderEntries = () => {
 };
 
 document.addEventListener('DOMContentLoaded', function(){
-    const createEntryForm = document.querySelector('#createEntryForm');
-    createEntryForm.addEventListener('submit', createEntry);
+    const createEntryForm = document.querySelector('#createForm');
+    createEntryForm.addEventListener('submit', createEntry, createBreak);
     indexEntries();
+    indexBreak();
 });
+
+
+const renderBreaks = () => {
+    const display = document.querySelector('#breaksDisplay');
+    display.innerHTML = '';
+    entries.forEach((brake) => {
+        const row = document.createElement('tr');
+        row.appendChild(createCell(brake.id));
+        row.appendChild(createCell(String(brake.hours)));
+        display.appendChild(row);
+    });
+};
+
+const createBreak = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const brake = {};
+    brake['hours'] = formData.get('breaktime');
+
+    fetch(`${URL}/breaks`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(brake)
+    }).then((result) => {
+        result.json().then((brake) => {
+            breaks.push(brake);
+            renderBreaks();
+        });
+    });
+};
+
+const indexBreak = () => {
+    fetch(`${URL}/breaks`, {
+        method: 'GET'
+    }).then((result) => {
+        result.json().then((result) => {
+            breaks = result;
+            renderBreaks();
+        });
+    });
+    renderBreaks();
+};
+
+
